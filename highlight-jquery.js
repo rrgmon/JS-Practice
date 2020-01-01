c973022c6128e157eeaba634885fe39dab1af7b5
RohitRegimon <rohit.2397@gmail.com>
2020-01-02 00:01:37 +0530

    First Commit

---
+++ highlight-jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#link-txtchange').click(function() {
+    $('#js1').effect('highlight', { color: '#rgb(70, 180, 248)' }, 1000);
+  });
+});
+
+$(document).ready(function() {
+  $('#link-disappear').click(function() {
+    $('#js2').effect('highlight', { color: '#rgb(70, 180, 248)' }, 1000);
+  });
+});
---
+++ script.js
@@ -0,0 +1,20 @@
+// Undo Changes
+
+function undo() {
+  var undoChange = document.getElementById('undo');
+  undoChange = window.location.reload();
+}
+
+//Changing the text in HTML
+
+function changeText() {
+  var txtChange = document.getElementById('textchange');
+  txtChange.innerHTML = 'This is the changed text. Pretty cool right!';
+}
+
+// Hiding an elementin HTML
+
+function disappearElement() {
+  var disElement = document.getElementById('disappear');
+  disElement.style.display = 'none';
+}
