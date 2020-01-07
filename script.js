85878ed86a0338a89d895828050d1b4850c44b0c
rrgmon <rrgmon@protonmail.com>
2020-01-07 11:18:42 +0530

    Changed JSPart to Flexbox from Grids.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Undo Changes
 
 function undo() {
-  var undoChange = document.getElementById('undo');
+  var undoChange = document.getElementById("undo");
   undoChange = window.location.reload();
 }
 
 //Changing the text in HTML
 
 function changeText() {
-  var txtChange = document.getElementById('textchange');
-  txtChange.innerHTML = 'This is the changed text. Pretty cool right!';
+  var txtChange = document.getElementById("textchange");
+  txtChange.innerHTML = "This is the changed text.";
 }
 
 // Hiding an elementin HTML
 
 function disappearElement() {
-  var disElement = document.getElementById('disappear');
-  disElement.style.display = 'none';
+  var disElement = document.getElementById("disappear");
+  disElement.style.display = "none";
 }
